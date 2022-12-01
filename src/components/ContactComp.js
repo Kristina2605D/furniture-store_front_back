72a4b274d71a3f19c84a0e82241c2ceb44560a8d
Kristina2605D <109177697+Kristina2605D@users.noreply.github.com>
2022-12-01 23:04:05 +0400

    'cart'

--- src/components/ContactComp.js
+++ src/components/ContactComp.js
@@ -6,49 +6,51 @@ const Contacts = () => {
     <section className='contact_us'>
       <div className='content_us'>
         <h2>Contact Us</h2>
-        <p>Lorem fkdkfdkmfkdmfkc</p>
+        
       </div>
       <div className='container_cont'>
         <div className='contact_info'>
           <div className='box_cont'>
-            <div className='icon_cont'><FiMapPin />
-              <div className='text_cont'>
-                <h3>Address</h3>
-                <p>hasce ,<br /></p>
-              </div>
-            </div>
-            <div className='box_cont'>
-              <div className='text_cont'>
-                <h3>Phone</h3><FiPhoneCall />
-                <p> 87484848484<br /></p>
-              </div>
+            <div className='icon_cont'><FiMapPin /></div>
+            <div className='text_cont'>
+              <h3>Address</h3>
+              <p>hasce <br /></p>
             </div>
           </div>
-          <div className='box'>
-            <div className='text'>
-              <h3>Email</h3><HiOutlineMailOpen />
+          <div className='box_cont'>
+            <div className='icon_cont'><FiPhoneCall /></div>
+            <div className='text_cont'>
+              <h3>Phone</h3>
+              <p> 87484848484<br /></p>
+            </div>
+          </div>
+
+          <div className='box_cont'>
+            <div className='icon_cont'><HiOutlineMailOpen /></div>
+            <div className='text_count'>
+              <h3>Email</h3>
               <p> wwwwww.@mail.ru<br /></p>
             </div>
           </div>
         </div>
         <div className='contactForm'>
           <form>
-            <h2>Semd Message</h2>
+            <h2>Send Message</h2>
             <div className='inputBox'>
-              <input type='text'name=''required/>
-               <span>Full Name</span>
-               </div>
-               <div className='inputBox'>
-              <input type='text'name=''required/>
-               <span>Email</span>
-               </div> 
-               <div className='inputBox'>
+              <input type='text_count' name='' required />
+              <span>Full Name</span>
+            </div>
+            <div className='inputBox'>
+              <input type='text_' name='' required />
+              <span>Email</span>
+            </div>
+            <div className='inputBox'>
               <textarea required></textarea>
-               <span>Type your Message...</span> 
-               </div>
-               <div className='inputBox'>
-               <input type='submit' name='' value='Send'/>
-               </div>
+              <span>Type your Message...</span>
+            </div>
+            <div className='inputBox'>
+              <input type='submit' name='' value='Send' />
+            </div>
           </form>
         </div>
       </div>
